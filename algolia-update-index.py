2b54e94809bfb614caf02b14468a8da44392551c
Luis Cacho <luiscachog@gmail.com>
2020-08-22 23:51:08 -0500

    added INPUT prefix to variables

--- algolia-update-index.py
+++ algolia-update-index.py
@@ -2,9 +2,9 @@ import json
 from algoliasearch.search_client import SearchClient
 from os import getenv
 
-ALGOLIA_APIKEY = getenv('ALGOLIA_APIKEY', 'none')
-ALGOLIA_APP_ID = getenv('ALGOLIA_APP_ID', 'none')
-ALGOLIA_INDEX_NAME = getenv('ALGOLIA_INDEX_NAME', 'none')
+ALGOLIA_APIKEY = getenv('INPUT_ALGOLIA_APIKEY', 'none')
+ALGOLIA_APP_ID = getenv('INPUT_ALGOLIA_APP_ID', 'none')
+ALGOLIA_INDEX_NAME = getenv('INPUT_ALGOLIA_INDEX_NAME', 'none')
 FILE = getenv('INPUT_FILE', 'none')
 
 client = SearchClient.create(ALGOLIA_APP_ID, ALGOLIA_APIKEY)
